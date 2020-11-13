2f1feafbfa87227682aee38ba936b4e81a2dc3d2
Cortez-Zhang <zhangzhujun36@gmail.com>
2020-11-13 19:41:23 +0800

    Update jd_joy_run.js
    fix token bug

--- jd_joy_run.js
+++ jd_joy_run.js
@@ -102,7 +102,8 @@ function getToken() {
   const url = $request.url;
   $.log(`${$.name}url\n${url}\n`)
   //if (isURL(url, /^https:\/\/draw\.jdfcloud\.com\/\/api\/user\/addUser\?code=\w+&/)) {
-  if (isURL(url, /^https:\/\/draw\.jdfcloud\.com\/mirror\/\/api\/user\/addUser\?code=\w+&/)) {
+  //if (isURL(url, /^https:\/\/draw\.jdfcloud\.com\/mirror\/\/api\/user\/addUser\?code=\w+&/)) {
+  if (isURL(url, /^https:\/\/draw\.jdfcloud\.com\/mirror\/\/api\/user\/addUser\?code=/)) {
     const body = JSON.parse($response.body);
     const LKYLToken = body.data.token;
     $.log(`${$.name} token\n${LKYLToken}\n`)
@@ -114,7 +115,8 @@ function getToken() {
     $.setdata(LKYLToken, 'jdJoyRunToken');
     $.done({ body: JSON.stringify(body) })
   //} else if (isURL(url, /^https:\/\/draw\.jdfcloud\.com\/\/api\/user\/user\/detail\?openId=\w+&/)){
-  } else if (isURL(url, /^https:\/\/draw\.jdfcloud\.com\/mirror\/\/api\/user\/user\/detail\?openId=\w+&/)){
+  //} else if (isURL(url, /^https:\/\/draw\.jdfcloud\.com\/mirror\/\/api\/user\/user\/detail\?openId=\w+&/)){
+  } else if (isURL(url, /^https:\/\/draw\.jdfcloud\.com\/mirror\/\/api\/user\/user\/detail\?openId=/)){
     if ($request && $request.method !== 'OPTIONS') {
       const LKYLToken = $request.headers['LKYLToken'];
       //if ($.getdata('jdJoyRunToken')) {
