ffa03cd0476798d8ca40330b7a8ff5f0c19cd607
Cortez <cortez@lab>
2020-12-16 20:35:14 +0800

    update

--- jd_jdh.js
+++ jd_jdh.js
@@ -39,8 +39,8 @@ if ($.isNode()) {
   cookiesArr.reverse();
 }
 const JD_API_HOST = 'https://api.m.jd.com/api';
+$.newShareCodes = [];
 !(async () => {
-  $.newShareCodes = [];
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
     return;
